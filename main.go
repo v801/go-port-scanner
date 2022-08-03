6832ae3e7c35ecceeb76d5ec24bfb3ac4bee04fa
v801 <32247825+v801@users.noreply.github.com>
2022-08-03 14:38:29 -0600

    move some code out into separate functions

--- main.go
+++ main.go
@@ -6,10 +6,33 @@ import (
 )
 
 func main() {
+	userPort := getUserInput()
+	isZero, isValidPortRange := validateUserInput(userPort)
+
+	if isZero {
+		fmt.Println("Select a port between 1-65535.")
+		return
+	}
+	if isValidPortRange {
+		fmt.Printf("Scanning TCP Ports 1-%v ...\n", userPort)
+		results := port.InitScan("localhost", userPort)
+		fmt.Printf("%+v\n", results)
+	} else {
+		fmt.Println("Select a port between 1-65535.")
+		return
+	}
+
+}
+
+func getUserInput() (int) {
 	var userPort int
 	fmt.Println("Enter upper port range:")
 	fmt.Scan(&userPort)
-	fmt.Println("Scanning Ports...")
-	results := port.InitScan("localhost", userPort)
-	fmt.Printf("%+v\n", results)
+	return userPort
+}
+
+func validateUserInput(userPort int) (bool, bool) {
+	isZero := userPort == 0
+	isValidPortRange := userPort >= 1 && userPort <= 65535
+	return isZero, isValidPortRange
 }
